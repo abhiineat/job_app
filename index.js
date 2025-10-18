1db30b6816f81225df1888bbacf3f2e97ac21912
abhiineat <abhii.neat@gmail.com>
2025-10-18 10:47:42 +0530

    prisma +postgres connect

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const prisma = require('./db');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send('Welcome to the Prisma Express API');
+});
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
